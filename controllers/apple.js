8b9be5320e9e4e70af026ba0b9a1b329f7a36aed
Murali-Kalla <kmuralidhar2019@gmail.com>
2021-11-10 16:09:52 -0600

    created implementation

--- controllers/apple.js
+++ controllers/apple.js
@@ -11,7 +11,8 @@ exports.apple_list = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
-};
+}; 
+
  
 // for a specific apple. 
 exports.apple_detail = function(req, res) { 
@@ -19,9 +20,38 @@ exports.apple_detail = function(req, res) {
 }; 
  
 // Handle apple create on POST. 
-exports.apple_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: apple create POST'); 
+// Handle apple create on POST. 
+exports.apple_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new apple(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"apple_type":"goat", "cost":12, "size":"large"} 
+    document.apple_type = req.body.apple_type; 
+    document.cost = req.body.cost; 
+    document.quantity = req.body.quantity; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
+// VIEWS 
+// Handle a show all view 
+exports.apple_view_all_Page = async function(req, res) { 
+    try{ 
+        theapple = await apple.find(); 
+        res.render('apple', { title: 'apple Search Results', results: theapple }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+};
  
 // Handle apple delete form on DELETE. 
 exports.apple_delete = function(req, res) { 
@@ -31,4 +61,4 @@ exports.apple_delete = function(req, res) {
 // Handle apple update form on PUT. 
 exports.apple_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: apple update PUT' + req.params.id); 
-}; 
+};
--- routes/apple.js
+++ routes/apple.js
@@ -1,9 +1,8 @@
 var express = require("express");
+const apple_controlers= require('../controllers/apple'); 
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("apple", { title: "Search Results Apple" });
-});
+router.get('/', apple_controlers.apple_view_all_Page);
 
 module.exports = router;
