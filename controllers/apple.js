e4fe6791a3972fa70b4e41d78d1b57c59599243c
Murali-Kalla <kmuralidhar2019@gmail.com>
2021-11-12 01:26:37 -0600

    Update changes

--- controllers/apple.js
+++ controllers/apple.js
@@ -66,6 +66,22 @@ exports.apple_delete = function(req, res) {
 }; 
  
 // Handle apple update form on PUT. 
-exports.apple_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: apple update PUT' + req.params.id); 
-};
+exports.apple_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await apple.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.apple_type)  
+               toUpdate.apple_type = req.body.apple_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.quantity) toUpdate.size = req.body.quantity; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
