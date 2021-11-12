d22ae22634b192ebd6c52a69ef026ea8e5d264da
Murali-Kalla <kmuralidhar2019@gmail.com>
2021-11-11 23:20:06 -0600

    added endpoints

--- controllers/apple.js
+++ controllers/apple.js
@@ -15,9 +15,16 @@ exports.apple_list = async function(req, res) {
 
  
 // for a specific apple. 
-exports.apple_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: apple detail: ' + req.params.id); 
-}; 
+exports.apple_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await apple.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};  
  
 // Handle apple create on POST. 
 // Handle apple create on POST. 
