4de97acf14bf6681ed025429210371a9e3166d34
Murali-Kalla <kmuralidhar2019@gmail.com>
2021-11-17 16:44:55 -0600

    created page view for deleting

--- controllers/apple.js
+++ controllers/apple.js
@@ -109,3 +109,45 @@ exports.apple_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for creating a apple.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.apple_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('applecreate', { title: 'apple Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a apple. 
+// query provides the id 
+exports.apple_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await apple.findById(req.query.id) 
+        res.render('appleupdate', { title: 'apple Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+// Handle a delete one view with id from query 
+exports.apple_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await apple.findById(req.query.id) 
+        res.render('appledelete', { title: 'apple Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
--- routes/apple.js
+++ routes/apple.js
@@ -6,5 +6,13 @@ var router = express.Router();
 router.get('/', apple_controlers.apple_view_all_Page);
 /* GET detail apple page */ 
 router.get('/detail', apple_controlers.apple_view_one_Page);
+/* GET create apple page */ 
+router.get('/create', apple_controlers.apple_create_Page)
+
+/* GET create update page */ 
+router.get('/update', apple_controlers.apple_update_Page);
+
+/* GET create apple page */ 
+router.get('/delete', apple_controlers.apple_delete_Page); 
 
 module.exports = router;
