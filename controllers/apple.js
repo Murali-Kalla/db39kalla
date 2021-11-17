ac64d1bcbccf75ede6303b76cbacac901ef121bb
Murali-Kalla <kmuralidhar2019@gmail.com>
2021-11-16 21:55:13 -0600

    created page view

--- controllers/apple.js
+++ controllers/apple.js
@@ -93,3 +93,19 @@ ${JSON.stringify(req.body)}`)
 failed`); 
     } 
 }; 
+
+// Handle a show one view with id specified by query
+exports.apple_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await apple.findById( req.query.id)
+        res.render('appledetail', {
+            title: 'apple Detail', 
+            toShow: result
+        });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/apple.js
+++ routes/apple.js
@@ -4,5 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', apple_controlers.apple_view_all_Page);
+/* GET detail apple page */ 
+router.get('/detail', apple_controlers.apple_view_one_Page);
 
 module.exports = router;
